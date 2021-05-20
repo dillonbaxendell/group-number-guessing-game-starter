8748372b09598f179f21821dd05b5858d69a72d6
James Kelley <jkkelley86@gmail.com>
2021-05-20 14:32:37 -0500

    POST info from players added to /guesses

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,38 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log("jquery is loaded!")
+  console.log("jquery is loaded!");
+
+  //click listener
+  $("#submit-button").on("click", handleSubmit);
+}
+
+function handleSubmit() {
+  console.log("In handle submit");
+
+  let newGuesses = {
+    player1: $("#jamesIn").val(),
+    player2: $("#dillonIn").val(),
+    player3: $("#owenIn").val(),
+    player4: $("#ianIn").val(),
+  };
+
+  // add to array
+  // push it /guesses
+  // MAKE A POST REQUEST with newGuesses
+  // Data should always be an Object
+
+  $.ajax({
+    method: "POST",
+    url: "/guesses",
+    // send data
+    data: newGuesses,
+  }).then((response) => {
+    console.log(response);
+    // need to clear inputs
+    $("#jamesIn").val("");
+    $("#dillonIn").val("");
+    $("#owenIn").val("");
+    $("#ianIn").val("");
+  });
 }
--- server/server.js
+++ server/server.js
@@ -9,15 +9,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static("server/public"));
 
-// GET & POST Routes go here
+//#region -- GET & POST Routes go here
+const guesses = require("./modules/guesses");
+app.get("/guesses", (req, res) => {
+  console.log("got to /guesses");
 
+  // respond
+  res.send(guesses);
+});
+
+app.post("/guesses", (req, res) => {
+  // We want to add the incoming data to /guesses
+  // req.body is made by body-parser
+  // info from client
+  console.log(req.body);
+  guesses.push(req.body);
+  // send back a good response
+  res.sendStatus(201);
+});
+//#endregion
+
+// THIS IS WHERE THE VOID LIVES
+
+//#region -- ARE YOU LISTENING?
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
 
-// Dillon
-// James
-// Owen
-//// HI ITS IAN!!!!! 
+//#endregion
 
 // test
