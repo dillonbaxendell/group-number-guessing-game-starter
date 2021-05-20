7f4e9bbc1d032ee41bf4be35d81e7a71296553bc
James Kelley <jkkelley86@gmail.com>
2021-05-20 16:25:44 -0500

    changed variable names

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,19 +2,46 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!");
-
+  getGuesses();
   //click listener
   $("#submit-button").on("click", handleSubmit);
+}
+
+function getGuesses() {
+  console.log("in getGuesses");
+  // Go to server route /guesses
+  // Promise that you'll come back
+  // Let's see what ajax has for us
+  $.ajax({
+    method: "GET",
+    url: "/guesses",
+  }).then(function (response) {
+    // response is what was in the res.send()
+    console.log(response);
+    // Need to empty
+    $("#player-choices").empty();
+    // Append information about guesses to DOM
+    for (let guess of response) {
+      $("#player-choices").append(`
+          <tr>
+              <td>${guess.player1}</td>
+              <td>${guess.player2}</td>
+              <td>${guess.player3}</td>
+              <td>${guess.player4}</td>
+          </tr>
+      `);
+    }
+  });
 }
 
 function handleSubmit() {
   console.log("In handle submit");
 
   let newGuesses = {
-    player1: $("#jamesIn").val(),
-    player2: $("#dillonIn").val(),
-    player3: $("#owenIn").val(),
-    player4: $("#ianIn").val(),
+    player1: $("#player-one").val(),
+    player2: $("#player-two").val(),
+    player3: $("#player-three").val(),
+    player4: $("#player-four").val(),
   };
 
   // add to array
@@ -30,9 +57,9 @@ function handleSubmit() {
   }).then((response) => {
     console.log(response);
     // need to clear inputs
-    $("#jamesIn").val("");
-    $("#dillonIn").val("");
-    $("#owenIn").val("");
-    $("#ianIn").val("");
+    $("#player-one").val("");
+    $("#player-two").val("");
+    $("#player-three").val("");
+    $("#player-four").val("");
   });
 }
